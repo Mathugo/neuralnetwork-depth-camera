29837348fc21026059b84e4dfc03fc55a9ba8fc8
hugo <hugo.mathh@gmail.com>
2022-05-24 14:59:37 +0200

    niryo now can work with positions

--- niryo/niryo.py
+++ niryo/niryo.py
@@ -2,24 +2,27 @@
 from niryo_one_tcp_client import *
 
 class Niryo:
-    def __init__(self, ip: str="10.10.10.10", grip: RobotTool= RobotTool.GRIPPER_2):
+    def __init__(self, ip: str="10.10.10.10", grip: RobotTool= RobotTool.GRIPPER_2, arm_velocity: int=30):
         self.n = NiryoOneClient()
         self.grip = grip
-        print("[!] Connecting to {}".format(ip))
-        self.n.connect(ip)  # =< Replace by robot ip address
-        print("[*] Done, starting auto calibration ..")
+        print("[!] Connecting to {} ..".format(ip))
+        self.n.connect(ip)
+        print("[*] Done")
+        print("[*] Calibrating the robot ..")
         status, data = self.n.calibrate(CalibrateMode.AUTO)
         if status:
             print("[*] Calibration done")
         else:
             print("[*] Impossible to calibrate")
-
+        self.set_arm_max_velocity(arm_velocity)
+        self.change_tool(self.grip)
         status, data = self.n.get_pose()
         if status is True:
             self.initial_pose = data
         else:
             print("Error: " + data)
             self.pose = None
+        self.position = (0.2, 0.0, 0.2, 0., 1.57, 0.0)
 
     @property
     def position(self):
@@ -30,49 +33,78 @@ class Niryo:
             return "Error"
     
     @position.setter
-    def position(self, x, y, z, roll, pitch, yaw):
-        status, data = self.n.move_joints(x, y, z, roll, pitch, yaw)
+    def position(self, value):
+        x, y, z, roll, pitch, yaw = value
+        status, data = self.n.move_pose(x, y, z, roll, pitch, yaw)
         if status is False:
             print("Error: " + data)
         else:
             print("[*] Moved successfully")
 
-    def increment_axis_x(self, value=0.10):
+
+    def calibration(self) -> None:
+        if self.n.need_calibration():
+            print("[!] Starting calibration ..")
+            status, data = self.n.calibrate(CalibrateMode.AUTO)
+            if status:
+                print("[*] Calibration done")
+            else:
+                print("[*] Impossible to calibrate")
+        else:
+            print("[*] No need to calibrate")
+    
+    def quit(self) -> None:
+        self.n.quit()
+
+    def set_arm_max_velocity(self, percentage: int) -> None:
+        print("[!] Setting {}% arm velocity".format(percentage))
+        if percentage > 100 or percentage < 0:
+            raise ValueError("Wrong percentage for velocity")  
+        else:
+            self.n.set_arm_max_velocity(percentage)
+            print("[*]  Done")
+
+    def change_tool(self, tool: RobotTool) -> None:
+        print("[*] Changing tool ..")
+        self.n.change_tool(tool)
+        print("[*] Done")
+
+    def increment_pos_x(self, value=0.10):
         status, data = self.n.shift_pose(RobotAxis.X, value)
         if status is False:
             print("Error: " + data)
         else:
             print("[*] Shifted correctly")
 
-    def increment_axis_y(self, value=0.15):
+    def increment_pos_y(self, value=0.15):
         status, data = self.n.shift_pose(RobotAxis.Y, value)
         if status is False:
             print("Error: " + data)
         else:
             print("[*] Shifted correctly")
     
-    def increment_axis_z(self, value=0.1):
+    def increment_pos_z(self, value=0.1):
         status, data = self.n.shift_pose(RobotAxis.Z, value)
         if status is False:
             print("Error: " + data)
         else:
             print("[*] Shifted correctly")
 
-    def increment_axis_pitch(self, value=0.2):
+    def increment_pos_pitch(self, value=0.2):
         status, data = self.n.shift_pose(RobotAxis.PITCH, value)
         if status is False:
             print("Error: " + data)
         else:
             print("[*] Shifted correctly")
 
-    def increment_axis_roll(self, value=0.3):
+    def increment_pos_roll(self, value=0.3):
         status, data = self.n.shift_pose(RobotAxis.ROLL, value)
         if status is False:
             print("Error: " + data)
         else:
             print("[*] Shifted correctly")
 
-    def increment_axis_yaw(self, value=0.3):
+    def increment_pos_yaw(self, value=0.3):
         status, data = self.n.shift_pose(RobotAxis.YAW, value)
         if status is False:
             print("Error: " + data)
@@ -95,19 +127,18 @@ class Niryo:
         )
         self.n.pick_from_pose(*pick_pose.to_list())
 
-    def place_object(self, x=0.25, y=0.0, z=0.14, roll=0.0, pitch=1.57, yaw=0.0):
+    def place_object(self, x=-0.01, y=0.23, z=0.12, roll=-0., pitch=1.57, yaw=-1.57):
         place_pose = PoseObject(
             x=x, y=y, z=z,
             roll=-roll, pitch=pitch, yaw=yaw,
         )
         self.n.place_from_pose(*place_pose.to_list())
 
-    def open_grippper(self, grip_speed=400):
+    def open_gripper(self, grip_speed=400):
         self.n.open_gripper(self.grip, grip_speed)
 
-    def close_grippper(self, grip_speed=400):
+    def close_gripper(self, grip_speed=400):
         self.n.close_gripper(self.grip, grip_speed)
-
 
 #n = Niryo()
 #print(n.position)
